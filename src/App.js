5a85a7ae3b22a3cd28fa59c90b1635dbabffe92d
petosbratok <44753312+petosbratok@users.noreply.github.com>
2022-10-29 00:01:12 +0300

    React components are ready. Design is in progress.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { ChatEngine } from 'react-chat-engine'
 
 import ChatFeed from './components/ChatFeed'
+import LoginForm from './components/LoginForm'
 
 import './App.css'
 
 const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />
+
   return (
     <ChatEngine
       height="100vh"
       projectID="e83822a7-8ebb-4000-843a-b50e1beea4e6"
-      userName="test_user1"
-      userSecret="123"
+      userName= {localStorage.getItem('username')}
+      userSecret= {localStorage.getItem('password')}
       renderChatFeed={(chatAppProps) => <ChatFeed {... chatAppProps} />}
     />
   )
--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -7,6 +7,17 @@ const ChatFeed = (props) => {
 
   const chat = chats && chats[activeChat];
 
+  const renderReadReceipts = (message, isMyMessage) => chat.people.map((person, index) => person.last_read === message.id && (
+      <div
+        key={`read_${index}`}
+        className="read-receipt"
+        style={{
+          float: isMyMessage? 'right': 'left',
+          backgroundImage: `url(${person?.person?.avatar})`
+        }}
+      />
+    ))
+
   const renderMessages = () => {
     const keys = Object.keys(messages);
     return keys.map((key, index) => {
@@ -24,7 +35,7 @@ const ChatFeed = (props) => {
             }
           </div>
           <div className="read-receipts" style={{marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0' : '68px'}}>
-            read-receipts
+            {renderReadReceipts(message, isMyMessage)}
           </div>
         </div>
       )
